61c91220968d1cdd3fc51a02c11e516c6e8bdb4f
Nick Jugganaikloo <nick.jugganaikloo@wavetronix.com>
2021-10-23 01:23:07 -0600

    Added Console Demo For Windows

---
+++ Tensorflow.Net.Lite.ConsoleDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tensorflowlite_c;
+
+namespace Tensorflow.Net.Lite.ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"TF Lite Version {c_api.TfLiteVersion()}");
+        }
+    }
+}
